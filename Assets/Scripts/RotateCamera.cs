f8baf7ba5fa841e40a7cb6239d775974b3735de1
Fulgore2002 <typryorhitchcock1@gmail.com>
2024-10-23 00:45:59 -0500

    Made a method return a spawn point

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 public class RotateCamera : MonoBehaviour
 {
     private float rotationSpeed = 50;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
