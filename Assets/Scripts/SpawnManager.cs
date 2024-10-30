cc73f3f4bc6f72e4ea8806d3a66c01d324159f31
Fulgore2002 <typryorhitchcock1@gmail.com>
2024-10-30 00:18:44 -0500

    Powerups spawn with new waves

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
     private float _spawnRange = 9.0f;
     public int _enemyCount;
     public int _waveNumber = 1;
@@ -12,17 +13,18 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SpawnEnemyWave(_waveNumber);    
+        SpawnEnemyWave(_waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
     void Update()
     {
         _enemyCount = FindObjectsOfType<Enemy>().Length; 
-
         if (_enemyCount == 0)
         {
             _waveNumber++;
             SpawnEnemyWave(_waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
         }
     }
 
